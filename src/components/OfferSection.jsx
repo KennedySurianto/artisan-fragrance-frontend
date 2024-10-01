34d676a7a64850bac047a6b2688a643d0778abd0
Kennedy Surianto <kennedysrnt@gmail.com>
2024-10-01 10:07:51 +0700

    modified: hero and offer section

--- src/components/OfferSection.jsx
+++ src/components/OfferSection.jsx
@@ -21,17 +21,17 @@ const OfferSection = () => {
     useSlideLeftAnimation(offerRefs2);
 
     return (
-        <section>
+        <section className="offer-section">
             <Offer 
-                title="Offer 1" 
-                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque, eveniet iste, laborum corrupti veritatis aliquam excepturi dolores illo consectetur obcaecati similique labore eos voluptas quos necessitatibus eaque repellat molestiae." 
-                imgUrl="https://picsum.photos/200" 
+                title="Exclusive Fragrance Sale: Get Your Signature Scent Today!" 
+                content="Discover the perfect scent that complements your style and leaves a lasting impression! For a limited time only, enjoy exclusive discounts on our best-selling fragrances. Whether you're looking for a sweet, long-lasting scent for daily wear or a bold fragrance for special occasions, we have something for every mood and moment." 
+                imgUrl="https://images.pexels.com/photos/965990/pexels-photo-965990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
                 ref={addToOfferRefs}
             />
             <Offer 
-                title="Offer 2" 
-                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, itaque, eveniet iste, laborum corrupti veritatis aliquam excepturi dolores illo consectetur obcaecati similique labore eos voluptas quos necessitatibus eaque repellat molestiae." 
-                imgUrl="https://picsum.photos/200" 
+                title="Feel the Difference: Sweet Fragrances That Last All Day!" 
+                content="Searching for a signature scent that’s both sweet and powerful? Look no further! Our expertly crafted perfumes are designed to captivate those around you, lasting up to 12 hours with incredible sillage that ensures you’ll never go unnoticed. Whether you're facing the heat or enjoying a cool night out, our fragrances adapt to your environment, keeping you fresh and confident all day long." 
+                imgUrl="https://images.pexels.com/photos/7405394/pexels-photo-7405394.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
                 ref={addToOfferRefs2}
             />
         </section>
